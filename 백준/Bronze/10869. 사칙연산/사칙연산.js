8ff217c086443431009df09bcc2d999efc163ff5
dlehdg <80308340+dlehdg@users.noreply.github.com>
2024-12-03 13:05:06 +0900

    [Bronze V] Title: 사칙연산, Time: 100 ms, Memory: 9600 KB -BaekjoonHub

--- 백준/Bronze/10869. 사칙연산/사칙연산.js
+++ 백준/Bronze/10869. 사칙연산/사칙연산.js
@@ -1,11 +1,13 @@
 let fs = require('fs');
 let input = fs.readFileSync('/dev/stdin').toString().split('\n');
 
-let a = Number(input[0].split(' ')[0]);
-let b = Number(input[0].split(' ')[1]);
+let number = input[0].split(' ');
 
-console.log(a + b);
-console.log(a - b);
-console.log(a * b);
-console.log(parseInt(a / b));
-console.log(a % b);
+let num1 = Number(number[0]);
+let num2 = Number(number[1]);
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(parseInt(num1 / num2));
+console.log(num1 % num2);
